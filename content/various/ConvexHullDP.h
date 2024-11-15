b5e6c947b66ccbd92f8c8512061d844393edf774
Xabier <xabier.sienra@gmail.com>
2024-11-15 00:02:42 -0500

    Updates

--- content/various/ConvexHullDP.h
+++ content/various/ConvexHullDP.h
@@ -6,7 +6,7 @@
  * value x and insertion must also efficient, the problem is equivalent to being given a set of lines 
  * and asked for the maximum y value any of line can give at a particular x. If you draw a bunch of 
  * straight lines on a plane, the maximums are along what appears to be a convex hull. So, a strategy
- * can be to only maintain the convex hull and not keep the useless lines. To querying at x = qi, 
+ * can be to only maintain the convex hull and not keep the useless lines. To querying at x = qi,
  * just compare the value at x of the rightmost line with that of the line next to it. If it is lower
  * remove it and repeat. When done, get the value at x of the rightmost line as the answer to the 
  * query. To insert a line, if the intersection point of this line and the leftmost line lies to the 
